5092bc77b0ceaf23a5bd3a24b11c056499fa83f9
Fil Mihaylov <filmihaylov1989@gmail.com>
2024-02-09 17:50:04 +0200

    Simple Inheritance

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -11,17 +11,31 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 
+// This is a typical set up of testing frameworks in which shared test logic is put in a base class
+// so it can be inherited of other tests
 public abstract class BaseTest {
 
-    protected  static FirefoxDriver driver;
+    // Here we specify the firefox driver that will be used to interact with FireFox browser
+    // it is protected, so it can be accessed only by classes with inherit the BaseTest class
+    protected static FirefoxDriver driver;
 
+    // Before Class is being called once before test class is run
     @BeforeClass
     public static void openBrowser(){
+        // Here we instantiate the Firefox Browser so we will be able to interact with it
+        // in our future tests
         driver = new FirefoxDriver();
+
+        //Here we set Implicitly Wait so if an element is not present in the dome immediately
+        // we will wait 10 seconds for it untill we will fail the execution of the tests.
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
+    // After Class is being called once after test class is run
     @AfterClass
+
+    // After running the tests we need to close the browser so we will
+    // not be left with multiple instancies of the browser
     public static void closeBrowser(){
         driver.quit();
     }
--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,25 +1,42 @@
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+// If we inherit from the base class the driver and BeforeClass and AfterClass will be run
+// This is our simple test that will check a login screen
 public class SeleniumTest extends BaseTest {
 
+    // This method is called before each test
+    @Before
+    public final void before() {
+
+
+    }
+
+    // this test will try to log in with valid credentials
+    // and verify that the user is logged in
     @Test
-    public void valid_UserCredential(){
+    public void validUserCredentialTest(){
         driver.get("https://www.saucedemo.com/");
     }
 
+    // this will be a negative test where we will try to log in with invalid credentials
+    // and will verify that we are not logged in
     @Test
-    public void inValid_UserCredential()
+    public void inValidUserCredentialTest()
     {
         driver.get("https://www.saucedemo.com/");
+    }
+
+    // This method is called after each test
+    @After
+    public final void after() {
+
+
     }
 
 }
